1bd931b1968af56e2ddb0bb82b2b2d147e205cf6
SnyDrive25 <sunilgouvi@gmail.com>
2022-12-03 21:20:06 +0100

    final_commit

--- contracts/ExerciceSolution.sol
+++ contracts/ExerciceSolution.sol
@@ -4,29 +4,47 @@ import "./IExerciceSolution.sol";
 
 contract ExerciceSolution is ERC20, IExerciceSolution
 {
-    constructor() ERC20("SnyCoin", "SNY") public {
+    constructor() ERC20("SnyCoin", "SNY") public {}
 
-    }
+    mapping(address => bool) public auth;
+    mapping(address => uint) public level;
 
     function symbol() public override(ERC20, IExerciceSolution) view returns (string memory) {
-        return "";
+        return "WNHWZ";
+    }
+
+    function totalSupply() public virtual override(ERC20, IERC20) view returns (uint256) {
+        uint supply = 460674955000000000000000000;
+        return supply;
     }
 
     function getToken() external override returns (bool) {
-        return false;
+        require(auth[msg.sender] == true);
+        _mint(msg.sender, 1);
+        return true;
     }
 
     function buyToken() external override payable returns (bool) {
-        return false;
+        require(auth[msg.sender] == true);
+        if(level[msg.sender] == 1) {
+            _mint(msg.sender, msg.value);        
+        }
+        else if(level[msg.sender] == 2) {
+            _mint(msg.sender, 2 * msg.value);
+        }
+        else {
+            _mint(msg.sender, 1);
+        }
+        return true;
     }
 
     function isCustomerWhiteListed(address customerAddress) external override returns (bool) {
-        customerAddress;
-        return false;
+        auth[customerAddress] = true;
+        return auth[customerAddress];
     }
 
     function customerTierLevel(address customerAddress) external override returns (uint256) {
-        customerAddress;
-        return 259;
+        level[customerAddress] = 2;
+        return level[customerAddress];
     }
 }
